5e389043fb422d02f169c0957b76fb1a00db5954
Bret Jackson <bjackson@macalester.edu>
2017-06-19 14:49:40 -0500

    Initial state implementation. Teleport works now

---
+++ Assets/Scripts/NavigationState.cs
@@ -0,0 +1,129 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NavigationState : InteractionState {
+
+    private int doNotTeleportLayer;
+    private int worldUILayer;
+
+    // Laser Pointer stuff
+    private GameObject laser;
+    private Transform laserTransform;
+    private Vector3 laserStartPos;
+    private Vector3 hitPoint;
+    private GameObject hitObject;
+    private int hitLayer;
+
+    // Teleport stuff
+    private Transform cameraRigTransform;
+    private GameObject reticle;
+    private Transform teleportReticleTransform;
+    private Transform headTransform;
+    private Vector3 teleportReticleOffset;
+    private bool shouldTeleport;
+
+
+    public NavigationState()
+    {
+        cameraRigTransform = GameObject.Find("[CameraRig]").transform;
+        headTransform = GameObject.FindGameObjectWithTag("MainCamera").transform;
+
+        teleportReticleOffset = new Vector3(0, 0.005f, 0);
+        doNotTeleportLayer = 9;
+        worldUILayer = 10;
+
+        laser = Instantiate(Resources.Load<GameObject>("Prefabs/LaserPointer"));
+        laserTransform = laser.transform;
+
+        reticle = Instantiate(Resources.Load<GameObject>("Prefabs/Reticle"));
+        teleportReticleTransform = reticle.transform;
+    }
+
+    // Update is called once per frame
+    override public void HandleEvents(ControllerInfo controller0Info, ControllerInfo controller1Info)
+    {
+        // Teleport
+        if (controller0Info.device.GetHairTrigger()) {
+            print("controller0 trigger down in update");
+            DoRayCast(controller0Info);
+        }
+        else if (controller1Info.device.GetHairTrigger())
+        {
+            DoRayCast(controller1Info);
+            print("controller0 trigger down in update");
+        }
+        else if (controller0Info.device.GetHairTriggerUp() || controller1Info.device.GetHairTriggerUp())
+        {
+            if (shouldTeleport)
+            {
+                Teleport();
+            }
+        }
+        else
+        {
+            laser.SetActive(false);
+            reticle.SetActive(false);
+        }
+    }
+
+    void DoRayCast(ControllerInfo controllerInfo)
+    {
+        RaycastHit hit;
+
+        laserStartPos = controllerInfo.trackedObj.transform.position;
+        if (Physics.Raycast(laserStartPos, controllerInfo.trackedObj.transform.forward, out hit, 1000))
+        {
+            // No matter what object is hit, show the laser pointing to it
+            hitPoint = hit.point;
+            hitObject = hit.collider.gameObject;
+            hitLayer = hit.collider.gameObject.layer;
+            ShowLaser(hit);
+
+            if (hitLayer != doNotTeleportLayer && hitLayer != worldUILayer)
+            {
+                reticle.SetActive(true);
+                teleportReticleTransform.position = hitPoint + teleportReticleOffset;
+                shouldTeleport = true;
+            }
+            else
+            {
+                reticle.SetActive(false);
+                shouldTeleport = false;
+            }
+        }
+        else
+        {
+            laser.SetActive(false);
+            reticle.SetActive(false);
+            shouldTeleport = false;
+        }
+    }
+
+    private void Teleport()
+    {
+        // 1
+        shouldTeleport = false;
+        // 2
+        reticle.SetActive(false);
+        // 3
+        Vector3 difference = cameraRigTransform.position - headTransform.position;
+        // 4
+        difference.y = 0;
+        // 5
+        cameraRigTransform.position = hitPoint + difference;
+    }
+
+    private void ShowLaser(RaycastHit hit)
+    {
+        // 1
+        laser.SetActive(true);
+        // 2
+        laserTransform.position = Vector3.Lerp(laserStartPos, hitPoint, .5f);
+        // 3
+        laserTransform.LookAt(hitPoint);
+        // 4
+        laserTransform.localScale = new Vector3(laserTransform.localScale.x, laserTransform.localScale.y,
+            hit.distance);
+    }
+}
---
+++ Assets/Scripts/UIController.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractionState : MonoBehaviour
+{
+    private string name
+    {
+        get;
+        set;
+    }
+
+    public virtual void HandleEvents(ControllerInfo controller0Info, ControllerInfo controller1Info) { }
+
+}
+
+public class ControllerInfo
+{
+    public GameObject controller;
+    public bool isLeft;
+    public SteamVR_Controller.Device device;
+    public SteamVR_TrackedObject trackedObj;
+
+    public ControllerInfo (GameObject controller)
+    {
+        this.controller = controller;
+        isLeft = false;
+        trackedObj = controller.GetComponent<SteamVR_TrackedObject>();
+        Debug.Log((int)trackedObj.index + " trackedObj");
+        device = SteamVR_Controller.Input((int)trackedObj.index);
+        
+    }
+}
+
+public class UIController : MonoBehaviour {
+
+    private InteractionState currentState;
+    public GameObject controller0;
+    public GameObject controller1;
+    private ControllerInfo controller0Info;
+    private ControllerInfo controller1Info;
+
+	// Use this for initialization
+	void Start () {
+        //TODO initialized current state
+
+        SteamVR_TrackedObject trackedObj = controller0.GetComponent<SteamVR_TrackedObject>();
+        //print("Tracked Obj index: " + trackedObj.index);
+
+
+        controller0Info = new ControllerInfo(controller0);
+        controller1Info = new ControllerInfo(controller1);
+
+        currentState = new NavigationState();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        controller0Info = new ControllerInfo(controller0);
+        controller1Info = new ControllerInfo(controller1);
+
+        determineLeftRightControllers();
+        currentState.HandleEvents(controller0Info, controller1Info);
+	}
+
+    public void changeState(InteractionState newState)
+    {
+        currentState = newState;
+    }
+
+    void determineLeftRightControllers()
+    {
+        try
+        {
+            //print("Leftmost device index" + SteamVR_Controller.GetDeviceIndex(SteamVR_Controller.DeviceRelation.Leftmost));
+            //print("Tracked controller index: " + controller0Info.device.index);
+
+            if ((int) controller0Info.device.index == (SteamVR_Controller.GetDeviceIndex(SteamVR_Controller.DeviceRelation.Leftmost)))
+            {
+                controller0Info.isLeft = true;
+                controller1Info.isLeft = false;
+            }
+            else
+            {
+                controller0Info.isLeft = false;
+                controller1Info.isLeft = true;
+            }
+        }
+        catch(System.IndexOutOfRangeException e) {
+            print(e.Message);
+            controller0Info.isLeft = true;
+            controller1Info.isLeft = false;
+        }
+    }
+}
