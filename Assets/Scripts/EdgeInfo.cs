c486039d90ef48b692e66f199e3b612b11084bed
Bret Jackson <bjackson@macalester.edu>
2017-07-14 16:49:06 -0500

    Plane intersection works but is slow and has some bugs

--- Assets/Scripts/EdgeInfo.cs
+++ Assets/Scripts/EdgeInfo.cs
@@ -7,9 +7,21 @@ using UnityEngine;
 /// </summary>
 public class EdgeInfo {
     private int index0, index1;
-    //private List<Triangle> parentTriangles;
-    private Vector3 intersectionPoint;
-    private List<int> orderedIndices;
+
+    public EdgeInfo(int p0, int p1)
+    {
+        // Index0 is always the lower index.
+        if (p0 < p1)
+        {
+            index0 = p0;
+            index1 = p1;
+        }
+        else
+        {
+            index0 = p1;
+            index1 = p0;
+        }
+    }
 
     public int Index0
     {
@@ -21,45 +33,9 @@ public class EdgeInfo {
         get { return index1; }
     }
 
-    public Vector3 IntersectionPoint
-    {
-        get { return intersectionPoint; }
-        set { intersectionPoint = value; }
-    }
-
-    public List<int> OrderedIndices
-    {
-        get { return orderedIndices; }
-    }
-
-    public EdgeInfo(int p0, int p1)
-    {
-        index0 = p0;
-        index1 = p1;
-        orderedIndices = OrderIndices(new List<int> { index0, index1 });
-    }
-
-    //public void addParent(Triangle parent)
-    //{
-    //    parentTriangles.Add(parent);
-    //}
-
     public bool HasCommonIndexWith(EdgeInfo edge)
     {
         return this.index0 == edge.Index0 || this.index0 == edge.Index1 || this.index1 == edge.Index0 || this.index1 == edge.Index1;
-    }
-
-    // Orders index0 and index1 of edge, small to large
-    public static List<int> OrderIndices(List<int> edge)
-    {
-        if (edge[1] < edge[0])
-        {
-            return new List<int> { edge[1], edge[0] };
-        }
-        else
-        {
-            return edge;
-        }
     }
 
     public override bool Equals(object obj)
@@ -67,5 +43,10 @@ public class EdgeInfo {
         EdgeInfo otherObj = (EdgeInfo)obj;
 
         return (otherObj.index0 == this.index0 && otherObj.index1 == this.index1) || (otherObj.index1 == this.index0 && otherObj.index0 == this.index1);
+    }
+
+    public override int GetHashCode()
+    {
+        return index0.GetHashCode() + index1.GetHashCode();
     }
 }
--- Assets/Scripts/PlaneCollision.cs
+++ Assets/Scripts/PlaneCollision.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using UnityEngine;
 
 public class PlaneCollision : MonoBehaviour
@@ -11,6 +12,8 @@ public class PlaneCollision : MonoBehaviour
     private HashSet<GameObject> crossSections;
     private Dictionary<List<Vector3>, GameObject> processedPoints;
 
+    private Boolean firstTime;
+
     //private GameObject crossSection;
 
     // Use this for initialization
@@ -18,14 +21,17 @@ public class PlaneCollision : MonoBehaviour
     {
         meshObjects = new HashSet<Collider>();
         crossSections = new HashSet<GameObject>();
+
+        firstTime = false;
     }
 
     // Update is called once per frame
     void Update()
     {
         processedPoints = processMeshes();
-        if (processedPoints.Count != 0)
+        if (processedPoints.Count != 0)// && firstTime == false)
         {
+            firstTime = true;
             // for each of the ordered lists of points (1 per intersected mesh), draw them in a new game object
             foreach (List<Vector3> meshPoints in processedPoints.Keys)
             {
@@ -34,6 +40,13 @@ public class PlaneCollision : MonoBehaviour
                 crossSection.transform.position = processedPoints[meshPoints].transform.position;
                 crossSection.transform.localScale = processedPoints[meshPoints].transform.localScale;
                 crossSection.transform.rotation = processedPoints[meshPoints].transform.rotation;   // or localrotation?
+
+                //foreach (Vector3 pt in meshPoints)
+                //{
+                //    GameObject sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+                //    sphere.transform.position = pt;
+                //    sphere.transform.localScale = new Vector3(0.05f, 0.05f, 0.05f);
+                //}
             }
         }
     }
@@ -63,20 +76,23 @@ public class PlaneCollision : MonoBehaviour
 
     public static bool ApproximatelyEquals(Vector3 vector0, Vector3 vector1)
     {
-        return (Mathf.Approximately(vector0.x, vector1.x) && Mathf.Approximately(vector0.y, vector1.y) && Mathf.Approximately(vector0.z, vector1.z));
+        float epsilon = 0.00001f;
+
+        return (Mathf.Abs(vector0.x - vector1.x) < epsilon && Mathf.Abs(vector0.y - vector1.y) < epsilon && Mathf.Abs(vector0.z- vector1.z) < epsilon);
     }
 
     // Orders the points of one mesh. NOTE: currently just uses alreadyVisited HashSet, nothing else;
-    List<Vector3> BFSOrderPoints(Dictionary<int, HashSet<int>> pointConnections, List<IntersectingPoint> actualPoints)
+    List<Vector3> DFSOrderPoints(Dictionary<int, HashSet<int>> pointConnections, List<IntersectingPoint> actualPoints)
     {
         HashSet<int> alreadyVisited = new HashSet<int>();
         List<Vector3> orderedPoints = new List<Vector3>();
-         
+
         foreach (int pt in pointConnections.Keys)
         {
             if (!alreadyVisited.Contains(pt))
             {
-                BFSVisit(pt, pointConnections, ref alreadyVisited, ref orderedPoints, actualPoints);
+                //TODO: make a new list for ordered points here to pass in
+                DFSVisit(pt, pointConnections, ref alreadyVisited, ref orderedPoints, actualPoints);
             }
         }
 
@@ -84,26 +100,26 @@ public class PlaneCollision : MonoBehaviour
     }
 
     // Basic BFS, adds the intersection points of edges in the order it visits them
-    void BFSVisit(int pt, Dictionary<int, HashSet<int>> connectedEdges, ref HashSet<int> alreadyVisited, ref List<Vector3> orderedPoints, List<IntersectingPoint> actualPoints)
+    void DFSVisit(int pt, Dictionary<int, HashSet<int>> connectedEdges, ref HashSet<int> alreadyVisited, ref List<Vector3> orderedPoints, List<IntersectingPoint> actualPoints)
     {
         alreadyVisited.Add(pt);
         orderedPoints.Add(actualPoints.ElementAt(pt).Point);
-        Queue<int> nextUp = new Queue<int>();
-        
-        nextUp.Enqueue(pt);
-        while (nextUp.Count != 0)
+        //Stack<int> nextUp = new Stack<int>();
+
+        //nextUp.Push(pt);
+        //while (nextUp.Count != 0)
+        //{
+            //int currentIndex = nextUp.Pop();
+        foreach (int otherIndex in connectedEdges[pt])//[currentIndex])
         {
-            int currentIndex = nextUp.Dequeue();
-            foreach (int otherIndex in connectedEdges[currentIndex])
+            if (!alreadyVisited.Contains(otherIndex))
             {
-                if (!alreadyVisited.Contains(otherIndex))
-                {
-                    alreadyVisited.Add(otherIndex);
-                    orderedPoints.Add(actualPoints.ElementAt(otherIndex).Point);
-                    nextUp.Enqueue(otherIndex);
-                }
+              
+            //nextUp.Push(otherIndex);
+                DFSVisit(otherIndex, connectedEdges, ref alreadyVisited, ref orderedPoints, actualPoints);
             }
         }
+        //}
     }
 
     Dictionary<List<Vector3>, GameObject> processMeshes() // each item in the list is a list of points corresponding to the intersection points of each mesh
@@ -121,8 +137,10 @@ public class PlaneCollision : MonoBehaviour
             mesh.GetVertices(vertices);
             int[] indices = mesh.GetTriangles(0); // 0 refers to first set of triangles (because there is only one material on the mesh)
 
-            List<EdgeInfo> intersectingEdges = new List<EdgeInfo>();
-         //   int intersectingPointIndex = 0;
+            Dictionary<EdgeInfo, Vector3> seenEdges = new Dictionary<EdgeInfo, Vector3>();
+
+            List<Vector3> intersectingEdges = new List<Vector3>();
+            //   int intersectingPointIndex = 0;
 
             for (int i = 0; i < indices.Length; i += 3)
             {
@@ -131,65 +149,91 @@ public class PlaneCollision : MonoBehaviour
                 //if (bound.Contains(m.transform.TransformPoint(vertices.ElementAt(indices[i]))) || bound.Contains(m.transform.TransformPoint(vertices.ElementAt(indices[i+1])))  || bound.Contains(m.transform.TransformPoint(vertices.ElementAt(indices[i+2]) ))) //m.transform.transformpoint does not work
                 //{
 
+                Vector3 intersectPoint0 = new Vector3();
+                Vector3 intersectPoint1 = new Vector3();
+                Vector3 intersectPoint2 = new Vector3();
+
                 EdgeInfo edge0 = new EdgeInfo(indices[i], indices[i + 1]);
-                EdgeInfo edge1 = new EdgeInfo(indices[i + 1], indices[i + 2]);
+                EdgeInfo edge1 = new EdgeInfo(indices[i+1], indices[i + 2]);
                 EdgeInfo edge2 = new EdgeInfo(indices[i], indices[i + 2]);
 
-                Vector3 intersectPoint = new Vector3();
+
+                //TODO: check if you have already processed an edge. If so, get it's intersection point.
 
                 // for each edge check if intersects with plane
                 // if 2 intersections then add the edge between them to the graph
                 // if 1 intersection then it goes through a vertex and we ignore it?
                 // if 3 (potentially through a vertex) then check which two are the same. Add edge to graph
 
-                if (intersectsWithPlane(vertices.ElementAt(edge0.Index0), vertices.ElementAt(edge0.Index1), m.gameObject, ref intersectPoint))
-                { 
-                    edge0.IntersectionPoint = intersectPoint;
-                    intersectPoint = new Vector3();
-                }
-                if (intersectsWithPlane(vertices.ElementAt(edge1.Index0), vertices.ElementAt(edge1.Index1), m.gameObject, ref intersectPoint))
+                bool side0;
+                if (seenEdges.TryGetValue(edge0, out intersectPoint0))
                 {
-                    edge1.IntersectionPoint = intersectPoint;
-                    intersectPoint = new Vector3();
+                    side0 = true;
                 }
-                if (intersectsWithPlane(vertices.ElementAt(edge2.Index0), vertices.ElementAt(edge2.Index1), m.gameObject, ref intersectPoint))
+                else
                 {
-                    edge2.IntersectionPoint = intersectPoint;
-                    intersectPoint = new Vector3();
+                    side0 = intersectsWithPlane(vertices.ElementAt(indices[i]), vertices.ElementAt(indices[i + 1]), m.gameObject, ref intersectPoint0);
+                    seenEdges.Add(edge0, intersectPoint0);
                 }
 
-                if (edge0.IntersectionPoint != null) { intersectingEdges.Add(edge0); }
-                if (edge1.IntersectionPoint != null) { intersectingEdges.Add(edge1); }
-                if (edge2.IntersectionPoint != null) { intersectingEdges.Add(edge2); }
-            
+                bool side1;
+                if (seenEdges.TryGetValue(edge1, out intersectPoint1))
+                {
+                    side1 = true;
+                }
+                else
+                {
+                    side1 = intersectsWithPlane(vertices.ElementAt(indices[i+1]), vertices.ElementAt(indices[i + 2]), m.gameObject, ref intersectPoint1);
+                    seenEdges.Add(edge1, intersectPoint1);
+                }
+
+                bool side2;
+                if (seenEdges.TryGetValue(edge2, out intersectPoint2))
+                {
+                    side2 = true;
+                }
+                else
+                {
+                    side2 = intersectsWithPlane(vertices.ElementAt(indices[i]), vertices.ElementAt(indices[i + 2]), m.gameObject, ref intersectPoint2);
+                    seenEdges.Add(edge2, intersectPoint2);
+                }
+
+
+                if (side0) { intersectingEdges.Add(intersectPoint0); }
+                if (side1) { intersectingEdges.Add(intersectPoint1); }
+                if (side2) { intersectingEdges.Add(intersectPoint2); }
+
                 switch (intersectingEdges.Count)
                 {
                     case 0:
+                        Debug.Log("0 edges hit");
                         break;
                     case 1:     // if 1 intersection then it goes through a vertex and we ignore it?
+                        Debug.Log("1 edge hit");
                         break;
                     case 2:     // if 2 intersections then add the edge between them to the graph
-                        
-                        AddToGraph(intersectingEdges.ElementAt(0).IntersectionPoint, intersectingEdges.ElementAt(1).IntersectionPoint, ref points, ref pointConnections);
 
+                        AddToGraph(intersectingEdges.ElementAt(0), intersectingEdges.ElementAt(1), ref points, ref pointConnections);
+                        Debug.Log("2 edges hit");
                         break;
                     case 3:     // if 3 (potentially through a vertex) then check which two are the same. Add edge to graph
-                        if (ApproximatelyEquals(edge0.IntersectionPoint, edge1.IntersectionPoint))
+                        if (ApproximatelyEquals(intersectPoint0, intersectPoint1))
                         {
-                            AddToGraph(edge0.IntersectionPoint, edge2.IntersectionPoint, ref points, ref pointConnections);
+                            AddToGraph(intersectPoint0, intersectPoint2, ref points, ref pointConnections);
                         }
-                        else if (ApproximatelyEquals(edge0.IntersectionPoint, edge2.IntersectionPoint))
+                        else if (ApproximatelyEquals(intersectPoint0, intersectPoint2))
                         {
-                            AddToGraph(edge0.IntersectionPoint, edge1.IntersectionPoint, ref points, ref pointConnections);
-                         
+                            AddToGraph(intersectPoint0, intersectPoint1, ref points, ref pointConnections);
+
                         }
                         else  // Edge1Point == Edge2 point
                         {
-                            AddToGraph(edge0.IntersectionPoint, edge1.IntersectionPoint, ref points, ref pointConnections);
+                            AddToGraph(intersectPoint0, intersectPoint1, ref points, ref pointConnections);
                         }
-                        
+                        Debug.Log("3 edges hit");       // NOTE: we are always hitting this case
                         break;
                 }
+                //}
 
                 intersectingEdges.Clear();
 
@@ -202,7 +246,16 @@ public class PlaneCollision : MonoBehaviour
                 //Debug.Log(m.transform.TransformPoint(vertices.ElementAt(indices[i])) + "point at " + i);
 
             }
-            List<Vector3> orderedMeshPoints = BFSOrderPoints(pointConnections, points);
+            List<Vector3> orderedMeshPoints = DFSOrderPoints(pointConnections, points);
+
+            StringBuilder builder = new StringBuilder();
+            foreach(Vector3 vec in orderedMeshPoints)
+            {
+                builder.Append(" ");
+                builder.Append(vec.ToString());
+            }
+
+            Debug.LogError(builder.ToString());
             intersectionPoints.Add(orderedMeshPoints, m.gameObject);
         }
         return intersectionPoints;
@@ -220,16 +273,26 @@ public class PlaneCollision : MonoBehaviour
         {                                                                       //
             index0 = points.Count;                                              //    
             points.Add(intersect0);                                             //
-        }                                                                       // Check if each point is already in List of points
+        } 
+        else
+        {
+            Debug.Log("p0 is already in the list");
+        }
+        // Check if each point is already in List of points
                                                                                 // If not, figure out what index it will be in then add it to list
                                                                                 // If so, simply find its index in the list
         if (index1 == -1)                                                       //
         {                                                                       //
             index1 = points.Count;                                              //
             points.Add(intersect1);                                             //
-        }                                                                       //
+        }
+        else
+        {
+            Debug.Log("p1 is already in the list");
+        }//
 
-        if (!pointConnections.ContainsKey(index0)){
+        if (!pointConnections.ContainsKey(index0))
+        {
             HashSet<int> connections = new HashSet<int>();
             connections.Add(index1);
             pointConnections.Add(index0, connections);
@@ -252,7 +315,7 @@ public class PlaneCollision : MonoBehaviour
     }
 
     private bool intersectsWithPlane(Vector3 lineVertex0, Vector3 lineVertex1, GameObject meshGameObject, ref Vector3 intersectPoint) // checks if a particular edge intersects with the plane, if true, returns point of intersection along edge
-    {
+    { // TODO: check xy of plane
         Vector3 lineVertex0World = meshGameObject.transform.TransformPoint(lineVertex0);
         Vector3 lineSegmentLocal = lineVertex1 - lineVertex0;
         Vector3 lineSegmentWorld = meshGameObject.transform.TransformPoint(lineVertex1) - lineVertex0World;
@@ -276,6 +339,9 @@ public class PlaneCollision : MonoBehaviour
 
     private Mesh CreateMesh(List<Vector3> points) // copied from EdgeSelectionState for testing purposes, not ideal for final version
     {
+        //TODO: This should actually take a list of lists. For each list you do the same thing we are doing below.
+        // indicies will need to be updated for each additional list.
+
         List<Vector3> vertices = new List<Vector3>();
         List<int> faces = new List<int>();
         List<Vector2> uvCoordinates = new List<Vector2>();
@@ -283,6 +349,8 @@ public class PlaneCollision : MonoBehaviour
         float radius = .005f;
 
         Vector3 previousRight = new Vector3(0, 0, 0);
+
+        //foreach (List<Vector3> points in allPoints)
 
         for (int i = 0; i < points.Count; i++)
         {
