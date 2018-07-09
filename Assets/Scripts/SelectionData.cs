44ad641891c555e0d17dee0114ea46de755c47a4
kaylabeckm <kaylabeckm@gmail.com>
2018-07-09 10:47:21 -0500

    added SelectionData class. TODO: update all of the selection interfaces to use this class.

---
+++ Assets/Scripts/SelectionData.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectionData : MonoBehavior {
+
+    void Start(){
+        
+    }
+
+    public SelectionData(){}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
